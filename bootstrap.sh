d644866de1ece8c8caf55abc892ad72ad4724f09
Jian-Hong Pan <chienhung.pan@gmail.com>
2022-07-13 23:26:37 +0800

    To have serial console

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,6 +13,9 @@ apk.static \
   --root $ROOT_TARGET \
   --initdb add alpine-base
 
+echo "Have serial console"
+sed -i '/\#ttyS0::respawn:\/sbin\/getty -L ttyS0 115200 vt100/a\ttyAMA0::respawn:\/sbin\/getty -L 0 ttyAMA0 vt100' $ROOT_TARGET/etc/inittab
+
 echo "Deploy fstab"
 install -D data/fstab $ROOT_TARGET/etc/fstab
 mkdir $ROOT_TARGET/boot
